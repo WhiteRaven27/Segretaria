7be7b6de02361740441ed1ab70abef5b2cefdfd5
WhiteRaven27 <manakyokochan@gmail.com>
2026-06-03 12:44:43 +0200

    Add message_content intent to fix warning

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ from discord.ext import commands
 
 
 intents = discord.Intents.default()
+intents.message_content = True  # Enable message content intent
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # =========================
